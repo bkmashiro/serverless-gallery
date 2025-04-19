84f0b505fedf523f05b895c5510a69d402ad99cb
bkmashiro <53376445+bkmashiro@users.noreply.github.com>
2025-04-19 18:34:06 +0100

    add type defs

---
+++ shared/types/index.ts
@@ -0,0 +1,22 @@
+export interface ImageMetadata {
+    id: string;           // Image filename (primary key)
+    photographer?: string; // Photographer's name
+    date?: string;        // Date of the photo
+    caption?: string;     // Photo caption
+    status?: string;      // Pass or Reject
+    reason?: string;      // Reason for status decision
+}
+
+export interface MetadataMessage {
+    id: string;
+    value: string;
+}
+
+export interface StatusUpdateMessage {
+    id: string;
+    date: string;
+    update: {
+        status: 'Pass' | 'Reject';
+        reason: string;
+    }
+} 
